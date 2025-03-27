afbf2ff2ef07a09673ff7df0892a5c521a41fe96
Azamat047 <bolathanovazamat047@gmail.com>
2025-03-28 00:52:33 +0500

    Completed CustomCircularProgressBar (Bugs in Future)

--- app/src/main/java/com/example/tasktracker/composables/CustomCircularProgressBar.kt
+++ app/src/main/java/com/example/tasktracker/composables/CustomCircularProgressBar.kt
@@ -1,11 +1,64 @@
 package com.example.tasktracker.composables
 
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun CustomCircularProgressBar (
-
+    finalNumber: Int,
+    currentNumber: Int,
+    fontSize: TextUnit = 24.sp,
+    radius: Dp = 50.dp,
+    color: Color = Color.Blue,
+    strokeWidth: Dp = 8.dp,
+    animDuration: Int = 1000
 ) {
+    val currentPercentage = animateFloatAsState(
+        targetValue = (finalNumber.toFloat())/(currentNumber.toFloat()),
+        animationSpec = tween(durationMillis = animDuration)
+    )
 
+    Box (
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.size(radius * 2f)
+    ) {
+        Canvas (modifier = Modifier.size(radius * 2f)) {
+            drawArc(
+                color = color,
+                startAngle = -90f,
+                sweepAngle = 360f * (currentNumber - 1),
+                useCenter = false,
+                style = Stroke(strokeWidth.toPx(), cap = StrokeCap.Round)
+            )
+            drawArc(
+                color = color,
+                startAngle = 360f * (currentNumber - 1),
+                sweepAngle = currentPercentage.value,
+                useCenter = false,
+                style = Stroke(strokeWidth.toPx(), cap = StrokeCap.Round)
+            )
+        }
+        Text(
+            text = "16:00",
+            fontSize = fontSize
+        )
+    }
 }
